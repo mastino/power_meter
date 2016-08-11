e9d430735809ea5476096a10589c47fb2c2d26d9
Rickie Kerndt <kerndtr@kerndt.com>
2016-08-11 14:37:38 -0700

    rework to run two ina219 meters, one for monitor line power and one for battery power

--- data_aquisition.py
+++ data_aquisition.py
@@ -3,44 +3,57 @@ Rickie Kerndt <rkerndt@cs.uoregon.edu>
 """
 from __future__ import print_function
 import power_meter as pm
+import ina219
 import datetime
 from Queue import Empty
 from sys import argv
 
-def main(serial_port, output_fh, duration):
+def main(pc_output_fh, line_output_fh, duration):
 
-    meter = pm.PowerMeter(serial_port)
+    pc_monitor = pm.INA219_Monitor(1.0, 0x40, 1, ina219.INA219_CALIB_32V_1A)
+    line_monitor = pm.INA219_Monitor(1.0, 0x41, 1, ina219.INA219_CALIB_32V_1A)
+    pc_meter = pm.PowerMeter(pc_monitor)
+    line_meter = pm.PowerMeter(line_monitor)
+
     start = datetime.datetime.now()
-    meter.open()
+    pc_meter.open()
+    line_meter.open()
+
+
     header = ','.join([pm.PowerData.csv_header(), 'wattSeconds'])
     print(header)
     while (datetime.datetime.now() - start < duration):
         try:
-            power = meter.next(False)
+            pc_power = pc_meter.next(False)
         except Empty :
-            power = None
-        if power:
-            line = ','.join([power.csv(),str(meter._calib_watt_seconds)])
-            print(power)
-            print(line, file=output_fh)
+            pc_power = None
+        try:
+            line_power = line_meter.next(False)
+        except Empty:
+            line_power = None
+        if pc_power:
+            line = ','.join([pc_power.csv(),str(pc_meter._calib_watt_seconds)])
+            print('pc', pc_power)
+            print(line, file=pc_output_fh)
+        if line_power:
+            line = ','.join([line_power.csv(),str(line_meter._calib_watt_seconds)])
+            print('line', line_power)
+            print(line, file=line_output_fh)
 
 if __name__ == '__main__':
 
     if len(argv) >= 2:
         file_name = argv[1]
     else:
-        file_name = 'power_measurment.csv'
-    fh = open(file_name, 'w')
+        file_name = 'power_measurment'
+
+    pc_fh = open(file_name+'_pc.csv', 'w')
+    line_fh = open(file_name+'_line.csv', 'w')
 
     if len(argv) >= 3:
-        port = argv[2]
-    else:
-        port = '/dev/tty.usbserial-A1057F91'
-
-    if len(argv) >= 4:
         duration = datetime.timedelta(minutes=argv[2])
     else:
         duration = datetime.timedelta(minutes=5)
 
 
-    main(port, fh, duration)
+    main(pc_fh, line_fh, duration)
